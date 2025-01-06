013e9ba397fd07684e351b364572473ac92d1d86
pogyomo <pogyomo@gmail.com>
2025-01-07 04:16:53 +0900

    fix: respect neovim version for calling vim.validate (#41)

--- lua/submode/snapshot/keymap.lua
+++ lua/submode/snapshot/keymap.lua
@@ -17,17 +17,19 @@
 ---@field callback function?
 ---@field replace_keycodes boolean?
 
+local validate = require("submode.validate").validate
+
 ---@param mode ShortenMode
 ---@param buf  integer? Buffer handle or 0 for current buffer, or nil for global.
 ---@return KeymapInfo[]
 local function get_keymap(mode, buf)
-    vim.validate("mode", mode, function(s)
+    validate("mode", mode, function(s)
         -- stylua: ignore
         return vim.list_contains({
             "n", "v", "x", "s", "o", "i", "l", "c", "t", "!", ""
         }, s)
     end, "n, v, x, s, o, i, l, c, t, ! or ''")
-    vim.validate("buf", buf, { "number", "nil" })
+    validate("buf", buf, { "number", "nil" })
 
     local getter = function(m)
         if buf then
